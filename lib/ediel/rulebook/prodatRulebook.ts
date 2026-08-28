0fe7bd0d6cc132b8012285d4aa2924aa8707d44e
heke99 <129280077+heke99@users.noreply.github.com>
2026-08-28 09:40:43 +0200

    refactor(ediel): derive PRODAT appref from canonical process

--- lib/ediel/rulebook/prodatRulebook.ts
+++ lib/ediel/rulebook/prodatRulebook.ts
@@ -1,5 +1,8 @@
-import { evaluateApplicationReferenceGuard } from '@/lib/ediel/rulebook/canonicalRules'
 import { getCanonicalEdielError, type CanonicalEdielErrorKey } from '@/lib/ediel/rulebook/mapEdielError'
+import {
+  canonicalProdatApplicationReferenceForProcessGroup,
+  type ProdatApplicationReference,
+} from '@/lib/ediel/rulebook/prodatApplicationReference'
 import {
   allowedProdatSubtypes,
   resolveProdatSubtype,
@@ -34,7 +37,7 @@ export type ProdatCanonicalProfile = {
   profileKey: ProdatRuleProfileKey
   messageCode: ProdatMessageCode
   meaning: string
-  applicationReference: '23-DDQ-PRODAT' | '23-DGI-PRODAT'
+  applicationReference: ProdatApplicationReference
   associationAssignedCode: 'E2SE6A'
   edifactDirectory: 'D97A'
   guideVersion: '26.A'
@@ -73,6 +76,7 @@ function variants(code: ProdatMessageCode): { allowedVariants: string[]; subtype
 }
 
 function profile(input: Omit<ProdatCanonicalProfile,
+  | 'applicationReference'
   | 'associationAssignedCode'
   | 'edifactDirectory'
   | 'guideVersion'
@@ -83,7 +87,12 @@ function profile(input: Omit<ProdatCanonicalProfile,
   | 'allowedVariants'
   | 'subtypeRules'
 >): ProdatCanonicalProfile {
-  return { ...input, ...common, ...variants(input.messageCode) }
+  return {
+    ...input,
+    applicationReference: canonicalProdatApplicationReferenceForProcessGroup(input.processGroup),
+    ...common,
+    ...variants(input.messageCode),
+  }
 }
 
 /**
@@ -92,19 +101,19 @@ function profile(input: Omit<ProdatCanonicalProfile,
  * do not add a second hand-maintained combination matrix here.
  */
 export const PRODAT_CANONICAL_PROFILES: ProdatCanonicalProfile[] = [
-  profile({ profileKey: 'prodat_z01_customer_identity_request', messageCode: 'Z01', meaning: 'Request for customer identity', applicationReference: '23-DDQ-PRODAT', processGroup: 'customer_masterdata', direction: 'actor_to_portal', senderRole: 'supplier', receiverRole: 'grid_owner', z01AperakException: true, requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD'], errorKeys: ['OBJECT_NOT_IDENTIFIED', 'MANDATORY_FIELD_MISSING'] }),
-  profile({ profileKey: 'prodat_z02_customer_identity_response', messageCode: 'Z02', meaning: 'Response to customer identity request', applicationReference: '23-DDQ-PRODAT', processGroup: 'customer_masterdata', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'supplier', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD'], errorKeys: ['OBJECT_NOT_IDENTIFIED', 'MANDATORY_FIELD_MISSING'] }),
-  profile({ profileKey: 'prodat_z03_supplier_switch', messageCode: 'Z03', meaning: 'Supplier switch / move notification', applicationReference: '23-DDQ-PRODAT', processGroup: 'supplier_switch', direction: 'actor_to_portal', senderRole: 'supplier', receiverRole: 'grid_owner', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD+FR', 'NAD+DO', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED', 'INCORRECT_METERING_POINT_ID', 'INCORRECT_GRID_AREA_ID'] }),
-  profile({ profileKey: 'prodat_z04_supplier_switch_confirmation', messageCode: 'Z04', meaning: 'Grid-owner confirmation/information about supplier switch or move', applicationReference: '23-DDQ-PRODAT', processGroup: 'supplier_switch', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'supplier', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED'] }),
-  profile({ profileKey: 'prodat_z05_old_supplier_confirmation', messageCode: 'Z05', meaning: 'Grid-owner confirmation/information to old supplier', applicationReference: '23-DDQ-PRODAT', processGroup: 'supplier_switch', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'supplier', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED'] }),
-  profile({ profileKey: 'prodat_z06_masterdata_grid_to_supplier', messageCode: 'Z06', meaning: 'Grid-owner masterdata update to supplier', applicationReference: '23-DDQ-PRODAT', processGroup: 'masterdata', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'supplier', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED'] }),
-  profile({ profileKey: 'prodat_z08_contract_end', messageCode: 'Z08', meaning: 'Supplier information about ended/rescinded contract', applicationReference: '23-DDQ-PRODAT', processGroup: 'delivery_contract', direction: 'actor_to_portal', senderRole: 'supplier', receiverRole: 'grid_owner', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED'] }),
-  profile({ profileKey: 'prodat_z09_masterdata_supplier_to_grid', messageCode: 'Z09', meaning: 'Supplier masterdata update to grid owner', applicationReference: '23-DDQ-PRODAT', processGroup: 'masterdata', direction: 'actor_to_portal', senderRole: 'supplier', receiverRole: 'grid_owner', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED'] }),
-  profile({ profileKey: 'prodat_z10_meter_change', messageCode: 'Z10', meaning: 'Meter update from grid owner to supplier', applicationReference: '23-DDQ-PRODAT', processGroup: 'metering', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'supplier', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED'] }),
-  profile({ profileKey: 'prodat_z13_permission_request', messageCode: 'Z13', meaning: 'ESCO request for metering-value access', applicationReference: '23-DGI-PRODAT', processGroup: 'metering_access', direction: 'actor_to_portal', senderRole: 'esco', receiverRole: 'grid_owner', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD+FR', 'NAD+DO', 'NAD+UD', 'LIN', 'RFF+Z05'], errorKeys: ['OBJECT_NOT_IDENTIFIED', 'ACTOR_NOT_CONNECTED'] }),
-  profile({ profileKey: 'prodat_z14_permission_response', messageCode: 'Z14', meaning: 'Grid-owner approval/rejection of metering-value access', applicationReference: '23-DGI-PRODAT', processGroup: 'metering_access', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'esco', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD+FR', 'NAD+DO', 'NAD+UD', 'LIN', 'RFF+LI'], errorKeys: ['OBJECT_NOT_IDENTIFIED', 'INCORRECT_PERMISSION_STATUS'] }),
-  profile({ profileKey: 'prodat_z15_permission_ended', messageCode: 'Z15', meaning: 'Grid-owner termination of active metering-value permission', applicationReference: '23-DGI-PRODAT', processGroup: 'metering_access', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'esco', requiredSignals: ['NAD+FR', 'NAD+DO', 'NAD+UD', 'LIN', 'DTM+693', 'DTM+164', 'CCI+Z13', 'CCI+Z23', 'CCI+Z25', 'RFF+Z05', 'RFF+LI', 'RFF+Z09'], errorKeys: ['OBJECT_NOT_IDENTIFIED', 'INCORRECT_PERMISSION_STATUS', 'INCORRECT_PERMISSION_END_REASON'] }),
-  profile({ profileKey: 'prodat_z18_permission_end_request', messageCode: 'Z18', meaning: 'ESCO request to end metering-value reporting', applicationReference: '23-DGI-PRODAT', processGroup: 'metering_access', direction: 'actor_to_portal', senderRole: 'esco', receiverRole: 'grid_owner', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM+693', 'DTM+164', 'NAD+FR', 'NAD+DO', 'NAD+UD', 'LIN', 'CCI+Z13', 'CCI+Z25', 'RFF+Z05', 'RFF+LI', 'RFF+Z09'], errorKeys: ['INCORRECT_PERMISSION_END_REASON', 'ACTOR_NOT_CONNECTED'] }),
+  profile({ profileKey: 'prodat_z01_customer_identity_request', messageCode: 'Z01', meaning: 'Request for customer identity', processGroup: 'customer_masterdata', direction: 'actor_to_portal', senderRole: 'supplier', receiverRole: 'grid_owner', z01AperakException: true, requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD'], errorKeys: ['OBJECT_NOT_IDENTIFIED', 'MANDATORY_FIELD_MISSING'] }),
+  profile({ profileKey: 'prodat_z02_customer_identity_response', messageCode: 'Z02', meaning: 'Response to customer identity request', processGroup: 'customer_masterdata', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'supplier', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD'], errorKeys: ['OBJECT_NOT_IDENTIFIED', 'MANDATORY_FIELD_MISSING'] }),
+  profile({ profileKey: 'prodat_z03_supplier_switch', messageCode: 'Z03', meaning: 'Supplier switch / move notification', processGroup: 'supplier_switch', direction: 'actor_to_portal', senderRole: 'supplier', receiverRole: 'grid_owner', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD+FR', 'NAD+DO', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED', 'INCORRECT_METERING_POINT_ID', 'INCORRECT_GRID_AREA_ID'] }),
+  profile({ profileKey: 'prodat_z04_supplier_switch_confirmation', messageCode: 'Z04', meaning: 'Grid-owner confirmation/information about supplier switch or move', processGroup: 'supplier_switch', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'supplier', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED'] }),
+  profile({ profileKey: 'prodat_z05_old_supplier_confirmation', messageCode: 'Z05', meaning: 'Grid-owner confirmation/information to old supplier', processGroup: 'supplier_switch', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'supplier', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED'] }),
+  profile({ profileKey: 'prodat_z06_masterdata_grid_to_supplier', messageCode: 'Z06', meaning: 'Grid-owner masterdata update to supplier', processGroup: 'masterdata', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'supplier', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED'] }),
+  profile({ profileKey: 'prodat_z08_contract_end', messageCode: 'Z08', meaning: 'Supplier information about ended/rescinded contract', processGroup: 'delivery_contract', direction: 'actor_to_portal', senderRole: 'supplier', receiverRole: 'grid_owner', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED'] }),
+  profile({ profileKey: 'prodat_z09_masterdata_supplier_to_grid', messageCode: 'Z09', meaning: 'Supplier masterdata update to grid owner', processGroup: 'masterdata', direction: 'actor_to_portal', senderRole: 'supplier', receiverRole: 'grid_owner', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED'] }),
+  profile({ profileKey: 'prodat_z10_meter_change', messageCode: 'Z10', meaning: 'Meter update from grid owner to supplier', processGroup: 'metering', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'supplier', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD', 'LIN'], errorKeys: ['OBJECT_NOT_IDENTIFIED'] }),
+  profile({ profileKey: 'prodat_z13_permission_request', messageCode: 'Z13', meaning: 'ESCO request for metering-value access', processGroup: 'metering_access', direction: 'actor_to_portal', senderRole: 'esco', receiverRole: 'grid_owner', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD+FR', 'NAD+DO', 'NAD+UD', 'LIN', 'RFF+Z05'], errorKeys: ['OBJECT_NOT_IDENTIFIED', 'ACTOR_NOT_CONNECTED'] }),
+  profile({ profileKey: 'prodat_z14_permission_response', messageCode: 'Z14', meaning: 'Grid-owner approval/rejection of metering-value access', processGroup: 'metering_access', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'esco', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM', 'NAD+FR', 'NAD+DO', 'NAD+UD', 'LIN', 'RFF+LI'], errorKeys: ['OBJECT_NOT_IDENTIFIED', 'INCORRECT_PERMISSION_STATUS'] }),
+  profile({ profileKey: 'prodat_z15_permission_ended', messageCode: 'Z15', meaning: 'Grid-owner termination of active metering-value permission', processGroup: 'metering_access', direction: 'portal_to_actor', senderRole: 'grid_owner', receiverRole: 'esco', requiredSignals: ['NAD+FR', 'NAD+DO', 'NAD+UD', 'LIN', 'DTM+693', 'DTM+164', 'CCI+Z13', 'CCI+Z23', 'CCI+Z25', 'RFF+Z05', 'RFF+LI', 'RFF+Z09'], errorKeys: ['OBJECT_NOT_IDENTIFIED', 'INCORRECT_PERMISSION_STATUS', 'INCORRECT_PERMISSION_END_REASON'] }),
+  profile({ profileKey: 'prodat_z18_permission_end_request', messageCode: 'Z18', meaning: 'ESCO request to end metering-value reporting', processGroup: 'metering_access', direction: 'actor_to_portal', senderRole: 'esco', receiverRole: 'grid_owner', requiredSignals: ['UNB', 'UNH', 'BGM', 'DTM+693', 'DTM+164', 'NAD+FR', 'NAD+DO', 'NAD+UD', 'LIN', 'CCI+Z13', 'CCI+Z25', 'RFF+Z05', 'RFF+LI', 'RFF+Z09'], errorKeys: ['INCORRECT_PERMISSION_END_REASON', 'ACTOR_NOT_CONNECTED'] }),
 ]
 
 export function getCanonicalProdatProfile(messageCode: string | null | undefined): ProdatCanonicalProfile | null {
@@ -126,12 +135,27 @@ export function validateProdatSubtype(input: {
 
 export function validateProdatApplicationReference(input: { messageCode?: string | null; applicationReference?: string | null }) {
   const profile = getCanonicalProdatProfile(input.messageCode)
-  const guard = evaluateApplicationReferenceGuard({ family: 'PRODAT', messageCode: input.messageCode, applicationReference: input.applicationReference })
+  if (!profile) {
+    return {
+      ok: false,
+      expectedApplicationReference: null,
+      ruleKeys: ['APPREF_DDQ_FOR_SUPPLIER'],
+      reason: `PRODAT ${String(input.messageCode ?? '').toUpperCase() || 'utan meddelandekod'} ingår inte i den verifierade svenska 26.A-funktionslistan.`,
+    }
+  }
+
+  const provided = String(input.applicationReference ?? '').trim().toUpperCase()
+  const expected = profile.applicationReference
+  const ok = !provided || provided === expected
+  const ruleKey = profile.processGroup === 'metering_access'
+    ? 'APPREF_DGI_FOR_PERMISSION'
+    : 'APPREF_DDQ_FOR_SUPPLIER'
+
   return {
-    ok: guard.ok,
-    expectedApplicationReference: guard.expectedApplicationReference ?? profile?.applicationReference ?? null,
-    ruleKeys: guard.ruleKeys,
-    reason: guard.reason,
+    ok,
+    expectedApplicationReference: expected,
+    ruleKeys: ok ? [] : [ruleKey],
+    reason: ok ? null : `${profile.messageCode} ska använda ${expected}, inte ${input.applicationReference}.`,
   }
 }
 
